e7f500ac0913f66299482c04bb4647dba1a7139c
Martix4335 <martino.cerilli@gmail.com>
2023-04-07 22:41:22 +0200

    Versione dopo terzo incontro

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class FoglioCartaMillimetrata
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Goldenrod;
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(922, 679);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bottone di controllo";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictBox
+            // 
+            this.pictBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictBox.BackColor = System.Drawing.Color.FloralWhite;
+            this.pictBox.Location = new System.Drawing.Point(14, 17);
+            this.pictBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictBox.Name = "pictBox";
+            this.pictBox.Size = new System.Drawing.Size(1059, 654);
+            this.pictBox.TabIndex = 1;
+            this.pictBox.TabStop = false;
+            this.pictBox.SizeChanged += new System.EventHandler(this.pictBox_SizeChanged);
+            this.pictBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictBox_Paint);
+            // 
+            // FoglioCartaMillimetrata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1086, 743);
+            this.Controls.Add(this.pictBox);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FoglioCartaMillimetrata";
+            this.ShowIcon = false;
+            this.Text = "Foglio da disegno matematico";
+            ((System.ComponentModel.ISupportInitialize)(this.pictBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictBox;
+    }
+}
+
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class FoglioCartaMillimetrata : Form
+    {
+        public FoglioCartaMillimetrata()
+        {
+            InitializeComponent();
+            passo = 50;
+        }
+        int passo;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bottone di Prova");
+        }
+
+        private void pictBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics myGraphic = e.Graphics;
+
+            //nella riga qui di seguito converto 1 cm in pixel, così avrò un passo da 1 cm nella mia quadrettatura.
+            passo = Convert.ToInt32(myGraphic.DpiY / 2.54);
+
+            //di seguito imposto una penna personalizzata per disegnare il tratteggio. La userò per le linee di quadrettatura
+            Pen normalPen = new Pen(Color.Moccasin, 1.5f);
+            normalPen.DashStyle = DashStyle.Dash;
+
+            //il punto qui di seguito rappresenterà la coordinata dove disegnerò lo zero
+            PointF zero = new PointF();
+
+            //disegno linee verticali
+            for (int i = 0; i <= pictBox.Width / passo; i++)
+            {
+                myGraphic.DrawLine(normalPen, new Point(i * passo, 0), new Point(i * passo, pictBox.Height));
+
+                //la condizione if qui sotto l'ho 'inventata' per far si che una delle linee verticali disegnate sia più rimarcata e di colore diverso
+                //ho deciso di posizionarlaad 1/5 della larghezza del foglio, per motivi estetici.
+                //dentro l'if - come si vede dalle due condizioni logiche che ho imposto CHE DEVONO AVVENIRE IN CONTEMPORANEA (ho usato il connettore &&) - il ciclo for ci entra solo una volta
+                //e precisamente quando i è maggiore di (pictureBox1.Width / 5) / passo ma anche minore o uguale alla stessa cifra aumentata di 1. Altrimenti non esegue quella istruzione.
+                //vuol dire che: se ad esempio Width =20 e passo =2, allora se i > 2 e i<= 3 disegno la riga. Sto cioè disegnando la riga in coordinata 3!
+                if (i > (pictBox.Width / 5) / passo && i <= (1 + (pictBox.Width / 5) / passo))
+                {
+                    myGraphic.DrawLine(new Pen(Brushes.LightSalmon, 3f), new Point(i * passo, 0), new Point(i * passo, pictBox.Height));
+
+                    //qui sotto inserisco la coordinata x del punto dove disegnerò lo zero
+                    zero.X = i * passo + 5;
+                }
+            }
+
+            //stesso discorso per le linee orizzontali
+            for (int i = 0; i <= pictBox.Height / passo; i++)
+            {
+                myGraphic.DrawLine(normalPen, new Point(0, i * passo), new Point(pictBox.Width, i * passo));
+                if (i > (pictBox.Height / 2) / passo && i <= (1 + (pictBox.Height / 2) / passo))
+                {
+                    myGraphic.DrawLine(new Pen(Brushes.LightSalmon, 3f), new Point(0, i * passo), new Point(pictBox.Width, i * passo));
+
+                    //qui sotto inserisco la coordinata y del punto dove disegnerò lo zero
+                    zero.Y = i * passo + 5;
+                }
+            }
+            //la funzione qui di seguito è fantastica! ci permette di disegnare qualunque stringa noi volessimo sul nostro Graphics
+            myGraphic.DrawString("0", DefaultFont, Brushes.Red, zero);
+        }
+
+        private void pictBox_SizeChanged(object sender, EventArgs e)
+        {
+            pictBox.Invalidate(); //funzione importantissima!! Scatena il ri-disegno di tutto ciò che esiste dentro la picturebox
+        }
+    }
+}
